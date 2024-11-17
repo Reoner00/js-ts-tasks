69b45c7915a3b9299885f815fad017672d71c566
Reoner00 <piratrom4@gmail.com>
2024-11-17 21:55:34 +0200

    (feat): end-Hometas1

--- src/volumeCone.js
+++
@@ -1,9 +0,0 @@
-/**
- * Given cone height and radius (numbers). Evaluate volume of a cone
- * @param {number} h - height
- * @param {number} r - radius
- * @returns {number}
- */
-module.exports.volumeCone = function volumeCone(h, r) {
-  return ((1 / 3) * Math.PI * r ** 2 * h).toFixed(2);
-};
